4a738d3b6917438ffe195f488b107aa2603952fd
sivasanjaycse <sivasanjaidisco@gmail.com>
2025-03-13 16:04:23 -0700

    Updated the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,7 @@ import './Header.css'; // Importing the styles for the header
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="title">DigitalMess</h1>
-      <nav>
-        <ul className="nav-links">
-          <li><a href="/about" className="nav-link">About</a></li>
-          <li><a href="/services" className="nav-link">Services</a></li>
-        </ul>
-      </nav>
+      <h1 className="title">Digital Mess</h1>
     </header>
   );
 }
